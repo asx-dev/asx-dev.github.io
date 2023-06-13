8fb34f95c71d746143c83a5de8dfb00c1a486a4a
asx-dev <asxweb18@gmail.com>
2023-06-13 11:52:13 +0100

    scroll animations

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@
 const open = document.getElementById('menu__button')
 const nav = document.getElementById('mobile__nav')
 const close = document.getElementById('close__button')
+
 // Open and close menu
 open.addEventListener('click', toggleMobileMenu)
 close.addEventListener('click', toggleMobileMenu)
+
 function toggleMobileMenu() {
   nav.classList.toggle('hidden')
 }
@@ -23,3 +25,17 @@ function downloadResume() {
   link.download = 'asx-resume.pdf'
   link.click()
 }
+
+// Scroll Reveal Animation
+const sr = ScrollReveal({
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  reset: true,
+})
+
+sr.reveal('.home__content', { delay: 600, origin: 'bottom' })
+sr.reveal('.home__img', { delay: 600, origin: 'top' })
+sr.reveal('.skills__icons', { origin: 'top', interval: 300 })
+sr.reveal('.project__content', { origin: 'left' })
+sr.reveal('.project__img', { origin: 'right' })
