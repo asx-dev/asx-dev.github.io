bd673f1c26381528100ec368f6abe3f802fbbaa4
asx-dev <asxweb18@gmail.com>
2023-06-13 11:31:01 +0100

    resume download

--- js/script.js
+++ js/script.js
@@ -8,3 +8,18 @@ close.addEventListener('click', toggleMobileMenu)
 function toggleMobileMenu() {
   nav.classList.toggle('hidden')
 }
+
+// Resume
+const resumeBtn = document.getElementById('resumeBtn')
+const resumeMobile = document.getElementById('resumeMobile')
+
+// Add Events
+resumeBtn.addEventListener('click', downloadResume)
+resumeMobile.addEventListener('click', downloadResume)
+
+function downloadResume() {
+  let link = document.createElement('a')
+  link.href = '../public/resume.pdf'
+  link.download = 'asx-resume.pdf'
+  link.click()
+}
