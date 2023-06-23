34cf0a0d3ca1236af20a9ae67ca3f0d77fd1f6c7
asx-dev <asxweb18@gmail.com>
2023-06-23 19:43:20 +0100

    animation settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./*.html', './js/script.js'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        bounce: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-15%)' },
+        },
+      },
+      animation: {
+        bounce: 'bounce 3s infinite',
+      },
+    },
   },
   plugins: [],
 }
